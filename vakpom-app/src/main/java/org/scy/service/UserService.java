961f75bbd3c3ad943aa703c95900314d15adfc21
marcelsYK <sankponton@gmail.com>
2013-10-04 17:56:14 -0500

    Cleanup

--- vakpom-app/src/main/java/org/scy/service/UserService.java
+++ vakpom-app/src/main/java/org/scy/service/UserService.java
@@ -1,5 +1,73 @@
 package org.scy.service;
 
-public interface UserService {
+import java.util.List;
+import java.util.UUID;
 
+import org.scy.domain.User;
+import org.scy.repository.RoleRepository;
+import org.scy.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author ttts168
+ *
+ */
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	public User create(User user) {
+		user.setId(UUID.randomUUID().toString());
+		user.getRole().setId(UUID.randomUUID().toString());
+		
+		// We must save both separately since there is no cascading feature
+		// in Spring Data MongoDB (for now)
+		roleRepository.save(user.getRole());
+		return userRepository.save(user);
+	}
+	
+	public User read(User user) {
+		return user;
+	}
+	
+	public List<User> readAll() {
+		return userRepository.findAll();
+	}
+	
+	public User update(User user) {
+		User existingUser = userRepository.findByUsername(user.getUsername());
+		
+		if (existingUser == null) {
+			return null;
+		}
+		
+		existingUser.setFirstName(user.getFirstName());
+		existingUser.setLastName(user.getLastName());
+		existingUser.getRole().setRole(user.getRole().getRole());
+		
+		// We must save both separately since there is no cascading feature
+		// in Spring Data MongoDB (for now)
+		roleRepository.save(existingUser.getRole());
+		return userRepository.save(existingUser);
+	}
+	
+	public Boolean delete(User user) {
+		User existingUser = userRepository.findByUsername(user.getUsername());
+		
+		if (existingUser == null) {
+			return false;
+		}
+		
+		// We must delete both separately since there is no cascading feature
+		// in Spring Data MongoDB (for now)
+		roleRepository.delete(existingUser.getRole());
+		userRepository.delete(existingUser);
+		return true;
+	}
 }
